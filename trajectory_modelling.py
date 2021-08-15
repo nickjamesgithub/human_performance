c81dba77ead3d49c937641d4ecc4891f815a88d3
Nick James <87678037+nickjamesgithub@users.noreply.github.com>
2021-08-16 00:07:19 +1000

    Add files via upload
    Initial start with trajectory modelling script

---
+++ trajectory_modelling.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import glob
+
+path = '/Users/tassjames/Desktop/Olympic_data/olympic_data/field' # use your path
+all_files = glob.glob(path + "/*.csv")
+
+li = []
+li_specialised = []
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=None, header=0)
+    df_slice = df[["Rank", "Mark", "Nat", "gender", "event", "Date"]]
+    li.append(df)
+    li_specialised.append(df_slice)
+
+frame = pd.concat(li, axis=0, ignore_index=True)
+frame_sp = pd.concat(li_specialised, axis=0, ignore_index=True)
+
+# Change date format to just year
+
+# Grab best performance of each year
+
+# Plot trajectories
