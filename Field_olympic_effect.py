4f3ae12b08ee6a6265673324517f7e424bae60f4
Nick James <nicholas.james@sydney.edu.au>
2021-08-24 22:18:47 +1000

    update for new models and validation metrics

--- Field_olympic_effect.py
+++ Field_olympic_effect.py
@@ -8,7 +8,10 @@ from sklearn.metrics import mean_squared_error, r2_score
 import re
 import statsmodels.api as sm
 
-# Top 10/100
+# Turn plots on/off with True/False
+make_plots = False # True/False
+
+# Top 10/100 athletes
 top = 100 # 10/100
 
 path = '/Users/tassjames/Desktop/Olympic_data/olympic_data/field' # use your path
@@ -43,9 +46,11 @@ events_list_w = np.sort(events_list_w)
 
 # Loop over years of analysis
 years = np.linspace(2001,2019,19)
-rmse_list = []
+AIC_list = []
+BIC_list = []
 r2_list = []
-params_list = []
+pvals_list = []
+
 for g in range(len(genders)):
     for i in range(len(events_list_m)):
         # Slice a particular event
@@ -70,39 +75,23 @@ for g in range(len(genders)):
         combined = np.concatenate((x1,x2),axis=1)
         combined_ones = sm.tools.tools.add_constant(combined)
 
-        # Model 1
-        ols1 = LinearRegression(fit_intercept=True)
-        ols1.fit(x1, y)
-        int_1 = ols1.intercept_
-        coeffs_1 = ols1.coef_
-        m1_params = [int_1[0], coeffs_1[0][0]]
-        y_pred_1 = ols1.predict(x1)
-
         # Model 1 statsmodels
         model1 = sm.OLS(y, x1_ones)
         results1 = model1.fit()
-        print(results1.summary())
-        print("results parameters", results1.params)
-        print("AIC", results1.aic)
-        print("BIC", results1.bic)
-        print("Adjusted R2", results1.rsquared_adj)
-
-        # Model 2
-        ols2 = LinearRegression(fit_intercept=True)
-        ols2.fit(combined, y)
-        int_2 = ols2.intercept_
-        coeffs_2 = ols2.coef_
-        m2_params = [int_2[0], coeffs_2[0][0], coeffs_2[0][1]]
-        y_pred_2 = ols2.predict(combined)
+        # AIC/BIC/Adjusted R2
+        m1_aic = results1.aic
+        m1_bic = results1.bic
+        m1_r2a = results1.rsquared_adj
+        m1_pvals = results1.pvalues
 
         # Model 2 statsmodels
         model2 = sm.OLS(y, combined_ones)
         results2 = model2.fit()
-        print(results2.summary())
-        print("results parameters", results2.params)
-        print("AIC", results2.aic)
-        print("BIC", results2.bic)
-        print("Adjusted R2", results2.rsquared_adj)
+        # AIC/BIC/Adjusted R2
+        m2_aic = results2.aic
+        m2_bic = results2.bic
+        m2_r2a = results2.rsquared_adj
+        m2_pvals = results2.pvalues
 
         # relabel
         label = re.sub('[!@#$\/]', '', events_list_m[i])
@@ -112,33 +101,20 @@ for g in range(len(genders)):
         plt.plot(x1, results2.fittedvalues, label="model 2")
         plt.scatter(x1, y, label="data")
         plt.legend()
-        plt.savefig(label + gender_labels[g] + "_" + str(top)+"_SM")
+        plt.title(events_list_m[i]+"_"+gender_labels[g] + "_" + str(top))
+        plt.savefig(label + gender_labels[g] + "_" + str(top))
         plt.show()
 
-        # plot of regression
-        plt.scatter(x1, means)
-        plt.plot(x1, y_pred_1, label="Model 1", color="red")
-        plt.plot(x1, y_pred_2, label="Model 2", color="blue")
-        plt.scatter(x1, y, color="black", alpha=0.25)
-        plt.title(events_list_m[i]+"_"+gender_labels[g]+"_"+str(top))
-        plt.legend()
-        plt.savefig(label+gender_labels[g]+"_"+str(top))
-        plt.show()
-
-        # Compute RMSE and R2
-        rmse_m1 = np.sqrt(mean_squared_error(means, y_pred_1))
-        rmse_m2 = np.sqrt(mean_squared_error(means, y_pred_2))
-        r2_m1 = r2_score(means, y_pred_1)
-        r2_m2 = r2_score(means, y_pred_2)
-
-        # Append to lists
-        rmse_list.append([events_list_m[i] + "_" + gender_labels[g], rmse_m1, rmse_m2])
-        r2_list.append([events_list_m[i] + "_" + gender_labels[g], r2_m1, r2_m2])
-        params_list.append([events_list_m[i] + "_" + gender_labels[g], m1_params, m2_params])
+        # Append AIC/BIC/Adjusted R^2/p values to list
+        AIC_list.append([events_list_m[i] + "_" + gender_labels[g], m1_aic, m2_aic])
+        BIC_list.append([events_list_m[i] + "_" + gender_labels[g], m1_bic, m2_bic])
+        r2_list.append([events_list_m[i] + "_" + gender_labels[g], m1_r2a, m2_r2a])
+        pvals_list.append([events_list_m[i] + "_" + gender_labels[g], m1_pvals, m2_pvals])
 
 # Print RMSE and R2
-print(rmse_list)
+print(AIC_list)
+print(BIC_list)
 print(r2_list)
-print(params_list)
+print(pvals_list)
 
 
